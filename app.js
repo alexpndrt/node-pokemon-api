e0f163072e07decce255310697499a533eec8735
alexpndrt-macbook <alex.pondart@oclock.school>
2025-02-17 17:10:00 +0100

    ajout data pokemons fichier mock-pokemons

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
+let pokemons = require("./mock-pokemon");
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("hello, express 3 !"));
 app.get("/api/pokemons/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`Vous avez demandé le pokémon nº ${id}`);
+  const id = parseInt(req.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+  res.send(`Vous avez demandé le pokémon ${pokemon.name}`);
 });
 
 app.listen(port, () =>
