aa5340498d65c7d09e5bcc27dc56f861ab451b63
alexpndrt-macbook <alex.pondart@oclock.school>
2025-02-17 17:23:23 +0100

    création d'un nouveau endpoint - nombre total de pokémons

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("hello, express 3 !"));
+app.get("/api/pokemons", (req, res) => {
+  const numberPokemons = pokemons.length;
+  res.send(
+    `Il y a ${numberPokemons} Pokémons dans le pokédex, pour le moment.`
+  );
+});
 app.get("/api/pokemons/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
